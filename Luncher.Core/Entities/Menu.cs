dac097f4491193a2b3a4ced35fbda6a0d1236c8e
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2022-01-08 21:11:35 +0100

    ReadOnlyCollection

--- Luncher.Core/Entities/Menu.cs
+++ Luncher.Core/Entities/Menu.cs
@@ -1,20 +1,22 @@
-﻿namespace Luncher.Core.Entities
+﻿using System.Collections.ObjectModel;
+
+namespace Luncher.Core.Entities
 {
     public class Menu
     {
         private static readonly Lazy<Menu> empty = new(() => new(Array.Empty<Meal>(), Array.Empty<Soap>()));
 
-        public static Menu Create(ICollection<Meal> meals, ICollection<Soap> soaps) => new(meals, soaps);
+        public static Menu Create(IList<Meal> meals, IList<Soap> soaps) => new(meals, soaps);
 
         public static Menu Empty { get; } = empty.Value; //LazyThreadSafetyMode.ExecutionAndPublication
 
-        public Menu(ICollection<Meal> meals, ICollection<Soap> soaps)
+        public Menu(IList<Meal> meals, IList<Soap> soaps)
         {
-            Meals = meals;
-            Soaps = soaps;
+            Meals = new ReadOnlyCollection<Meal>(meals);
+            Soaps = new ReadOnlyCollection<Soap>(soaps);
         }
 
-        public ICollection<Meal> Meals { get; }
-        public ICollection<Soap> Soaps { get; }
+        public IReadOnlyCollection<Meal> Meals { get; }
+        public IReadOnlyCollection<Soap> Soaps { get; }
     }
 }
