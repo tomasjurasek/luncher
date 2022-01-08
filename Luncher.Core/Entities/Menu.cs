6703fef5de9a57d969d8ed7cf5f2caec7dd2d74c
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2022-01-08 21:12:26 +0100

    update

--- Luncher.Core/Entities/Menu.cs
+++ Luncher.Core/Entities/Menu.cs
@@ -4,11 +4,11 @@ namespace Luncher.Core.Entities
 {
     public class Menu
     {
-        private static readonly Lazy<Menu> empty = new(() => new(Array.Empty<Meal>(), Array.Empty<Soap>()));
+        private static readonly Lazy<Menu> _empty = new(() => new(Array.Empty<Meal>(), Array.Empty<Soap>()));
 
         public static Menu Create(IList<Meal> meals, IList<Soap> soaps) => new(meals, soaps);
 
-        public static Menu Empty { get; } = empty.Value; //LazyThreadSafetyMode.ExecutionAndPublication
+        public static Menu Empty { get; } = _empty.Value; //LazyThreadSafetyMode.ExecutionAndPublication
 
         public Menu(IList<Meal> meals, IList<Soap> soaps)
         {
