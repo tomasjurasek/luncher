f8cc17504a9b3603ca2ae962e89f8a7fe00a8325
Kamil Fňukal <kamil.fnukal@avast.com>
2022-08-01 15:21:43 +0200

    BILL-oběd

--- Luncher.Adapters.ThirdParty/Restaurants/PadowetzRestaurant.cs
+++ Luncher.Adapters.ThirdParty/Restaurants/PadowetzRestaurant.cs
@@ -3,13 +3,14 @@ using Luncher.Adapters.ThirdParty.Restaurants;
 using Luncher.Core.Entities;
 using Luncher.Domain.Entities;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Luncher.Adapters.ThirdParty
 {
     internal class PadowetzRestaurant : RestaurantBase
     {
         private readonly HtmlWeb _htmlWeb;
-        private string Url => $"http://www.restaurant-padowetz.cz/poledni-menu.htm";
+        private string Url => $"http://www.restaurant-padowetz.cz/poledni-menu.html";
 
         public PadowetzRestaurant() : base(RestaurantType.Padowetz)
         {
@@ -20,7 +21,7 @@ namespace Luncher.Adapters.ThirdParty
         protected override async Task<Restaurant> GetInfoCoreAsync(CancellationToken cancellationToken)
         {
             var htmlDocument = await _htmlWeb.LoadFromWebAsync(Url, cancellationToken);
-
+            /*
             var todayMenuNode = htmlDocument.DocumentNode.Descendants("div")
                 .Where(s => s.Attributes.Contains("class") && s.Attributes["class"].Value == "tydeniMenu ")
                 .First();
@@ -40,6 +41,27 @@ namespace Luncher.Adapters.ThirdParty
                 .Descendants("tr")
                 .Select(s => $"{s.ChildNodes[0].InnerText} {s.ChildNodes[1].InnerText}")
                 .Select(s => Meal.Create(s))
+                .ToList();
+            */
+
+            var todayMenuNode = htmlDocument.DocumentNode.Descendants("div")
+                .Where(s => s.Attributes.Contains("class") && s.Attributes["class"].Value == "col-md-7")
+                .ToList()[((int)DateTime.Today.DayOfWeek - 1) % 5];;
+
+            var soaps = todayMenuNode.Descendants("div")
+                .Where(s => s.Attributes.Contains("class") && s.Attributes["class"].Value == "col-sm-8 col-md-9")
+                .ToList()
+                .Select(s => s.InnerText)
+                .Select(Soap.Create)
+                .Take(2)
+                .ToList();
+
+            var meals = todayMenuNode.Descendants("div")
+                .Where(s => s.Attributes.Contains("class") && s.Attributes["class"].Value == "col-sm-8 col-md-9")
+                .ToList()
+                .Select(s => Regex.Replace(s.InnerText, @"\s+", " "))
+                .Select(Meal.Create)
+                .TakeLast(5)
                 .ToList();
 
             return Restaurant.Create(Type, Menu.Create(meals, soaps));
